1f2d3c66f3b78fc9359bdbba13fa07d2a9174375
彭皓 <hao.peng01@hand-china.com>
2021-04-04 14:24:03 +0800

    测试github上传

---
+++ src/main/java/com/ph/socialecurityvisualization/SocialSecurityVisualizationApplication.java
@@ -0,0 +1,16 @@
+package com.ph.socialecurityvisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hao.peng01@hand-china.com 2021/3/11 17:43
+ */
+@SpringBootApplication
+public class SocialSecurityVisualizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialSecurityVisualizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ph/socialecurityvisualization/SocialEcurityVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph.socialecurityvisualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialEcurityVisualizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
